a0f43c04ce2313a6b55475d5c8cfa919c5aca1e5
Aaahh <woohoomoo2u@gmail.com>
2014-10-22 22:30:44 -0400

    Copy logs to /system

---
+++ P.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo 'Checking folders...'
+if ! [ -d ./ramdisk/data ]; then
+    mkdir ./ramdisk/data
+fi
+if ! [ -d ./ramdisk/dev ]; then
+    mkdir ./ramdisk/dev
+fi
+if ! [ -d ./ramdisk/proc ]; then
+    mkdir ./ramdisk/proc
+fi
+if ! [ -d ./ramdisk/sys ]; then
+    mkdir ./ramdisk/sys
+fi
+if ! [ -d ./ramdisk/system ]; then
+    mkdir ./ramdisk/system
+fi
+echo 'Folders avaliabe'
+echo 'Packing Ramdisk...'
+cd ./ramdisk
+repack_ramdisk . ./ramdisk-boot
+echo 'Ramdisk Complete'
+cd ..
+echo 'Packing into Boot.img'
+mkbootimg --kernel boot.img-kernel --ramdisk ramdisk-boot -o boot.img
+echo 'Packing Complete.'
---
+++ ramdisk/sbin/logz.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+cp /proc/kmsg /system/kmsg
+cp /proc/last_kmsg /sytem/last_kmsg
