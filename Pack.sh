c604cd7af4c226151a5719aef7849be5f764e5e0
Aaahh <woohoomoo2u@gmail.com>
2014-10-25 21:55:31 -0400

    bacon and corrupt init

--- Pack.sh
+++ Pack.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo 'Checking folders...'
-if ! [ -d ./ramdisk/data ]; then
-    mkdir ./ramdisk/data
+if ! [ -d ./boot/ramdisk/data ]; then
+    mkdir ./boot/ramdisk/data
 fi
-if ! [ -d ./ramdisk/dev ]; then
-    mkdir ./ramdisk/dev
+if ! [ -d ./boot/ramdisk/dev ]; then
+    mkdir ./boot/ramdisk/dev
 fi
-if ! [ -d ./ramdisk/proc ]; then
-    mkdir ./ramdisk/proc
+if ! [ -d ./boot/ramdisk/proc ]; then
+    mkdir ./boot/ramdisk/proc
 fi
-if ! [ -d ./ramdisk/sys ]; then
-    mkdir ./ramdisk/sys
+if ! [ -d ./boot/ramdisk/sys ]; then
+    mkdir ./boot/ramdisk/sys
 fi
-if ! [ -d ./ramdisk/system ]; then
-    mkdir ./ramdisk/system
+if ! [ -d ./boot/ramdisk/system ]; then
+    mkdir ./boot/ramdisk/system
 fi
 echo 'Folders avaliabe'
 echo 'Packing into Boot.img'
